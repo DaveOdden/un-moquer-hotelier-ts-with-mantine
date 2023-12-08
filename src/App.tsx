662f7a6663750ebcae5dc40af6f97f2a19291bd4
Dave Odden <daveodden@Daves-MacBook-Pro.local>
2023-12-08 13:56:47 -0500

    start of ui with Mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route, Outlet } from 'react-router-dom'
+import { useDisclosure } from '@mantine/hooks'
+import { AppShell } from '@mantine/core'
 import './App.css'
+import { AppNav } from './components/AppNav'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	const [opened, { toggle }] = useDisclosure()
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<AppShell navbar={{ width: 80, breakpoint: 'sm', collapsed: { mobile: !opened } }} padding="md">
+			<AppShell.Navbar>
+				<AppNav />
+			</AppShell.Navbar>
+			<AppShell.Main>
+				<Routes>
+					<Route path="/" element={<MockLayout />}>
+						<Route index element={<h1>Test</h1>} />
+						<Route path="overview" element={<h1>Overview</h1>} />
+						<Route path="bookings" element={<h1>Bookings</h1>} />
+						<Route path="guests" element={<h1>Guests</h1>} />
+						<Route path="rooms" element={<h1>Rooms</h1>} />
+						<Route path="settings" element={<h1>Settings</h1>} />
+						<Route path="*" element={<h1>None</h1>} />
+					</Route>
+				</Routes>
+			</AppShell.Main>
+		</AppShell>
+	)
+}
+
+function MockLayout() {
+	return <Outlet />
 }
 
 export default App
