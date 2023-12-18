a97c4fe85ec3c1e4954434df6b27e03ee5676850
Dave Odden <daveodden@Daves-MacBook-Pro.local>
2023-12-17 19:37:36 -0500

    fix no shows

--- src/features/Dashboard/NoShows/NoShows.tsx
+++ src/features/Dashboard/NoShows/NoShows.tsx
@@ -1,7 +1,7 @@
 import { Text } from '@mantine/core'
 import { asDashboardCard } from '../asDashboardCard'
 
-const NoShowsContent: React.FC<{ title: string }> = ({ title }) => {
+const NoShowsContent: React.FC<{ title: string }> = () => {
 	return <Text>No Shows</Text>
 }
 export const NoShows = asDashboardCard(NoShowsContent)
