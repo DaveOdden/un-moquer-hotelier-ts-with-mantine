979efb18252f6f1d4cad9843ca334d425562677d
Dave Odden <daveodden@Daves-MacBook-Pro.local>
2023-12-22 22:02:34 -0500

    wire up alerts to api

--- src/features/Dashboard/Alerts/Alerts.tsx
+++ src/features/Dashboard/Alerts/Alerts.tsx
@@ -1,8 +1,24 @@
-import { Text } from '@mantine/core'
+import { Loader } from '@mantine/core'
+
+import { useAlerts } from 'src/hooks/useAlertsQuery'
 import { asDashboardCard } from '../asDashboardCard'
+import { AlertsListEmpty } from './Empty/AlertsListEmpty'
+import { AlertsList } from './List/AlertsList'
 
 export const AlertsContent: React.FC<{}> = () => {
-	return <Text>Alerts</Text>
+	const alerts = useAlerts()
+
+	if (alerts.isLoading) return <Loader />
+	if (alerts.isError) return <>Error</>
+
+	console.log(alerts.data)
+
+	return (
+		<>
+			<AlertsListEmpty isShown={alerts.data.length === 0} />
+			<AlertsList listData={alerts.data} />
+		</>
+	)
 }
 
 export const Alerts = asDashboardCard(AlertsContent)
