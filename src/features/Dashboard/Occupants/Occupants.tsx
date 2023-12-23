36141b8a02463587e0465b1fbcedcb5490821f48
Dave Odden <daveodden@Daves-MacBook-Pro.local>
2023-12-22 21:33:03 -0500

    occupants reorg

--- src/features/Dashboard/Occupants/Occupants.tsx
+++ src/features/Dashboard/Occupants/Occupants.tsx
@@ -1,42 +1,38 @@
+import { useState } from 'react'
 import { getAdditionalDataForEachBooking } from 'src/features/Bookings/utils'
 import { Loader, ScrollArea, Card, Text, Stack } from '@mantine/core'
+
 import { useAllFeatures } from 'src/hooks/useAllQuery'
 import { asDashboardCard } from '../asDashboardCard'
 import { IAggregatedBooking } from 'src/utils/types'
-import classes from './Occupants.module.css'
+import { OccupantsListEmpty } from './Empty/OccupantsListEmpty'
+import { OccupantsList } from './List/OccupantsList'
+import { OccupantModal } from './Modal/OccupantModal'
 
 export const OccupantsContent: React.FC<{}> = () => {
+	const [modalOpened, setModalOpened] = useState<boolean>(false)
 	const [guests, bookings, rooms] = useAllFeatures()
 	const aggregatedData = getAdditionalDataForEachBooking(guests, bookings, rooms)
 	const checkedInBookings = aggregatedData.filter(
 		(obj: IAggregatedBooking) => obj.checkedIn === true
 	)
 
+	const initializeModal = (record: any) => {
+		// setCheckinRecord(record)
+		openModal()
+	}
+	const openModal = () => setModalOpened(true)
+	const closeModal = () => setModalOpened(false)
+
 	if (bookings.isLoading) return <Loader />
 	if (bookings.isError) return <>Error</>
 
 	return (
-		<ScrollArea h="100%" className={classes.scrollArea}>
-			{checkedInBookings.map((booking) => (
-				<Card
-					py="md"
-					px="lg"
-					w="100%"
-					shadow="none"
-					radius={0}
-					key={booking.guest._id}
-					style={{ flexShrink: 0, borderBottom: `1px solid var(--mantine-color-gray-3)` }}>
-					<Stack justify="flex-start" gap={0} align="flex-start">
-						<Text fw={500} c="dark.4" lineClamp={1}>
-							{booking.guest.fullName}
-						</Text>
-						<Text fw={500} c="dimmed" size="sm" lineClamp={1}>
-							Room #{booking.room.roomNum}
-						</Text>
-					</Stack>
-				</Card>
-			))}
-		</ScrollArea>
+		<>
+			<OccupantsListEmpty isShown={checkedInBookings.length === 0} />
+			<OccupantsList listData={checkedInBookings} showCheckInModal={initializeModal} />
+			<OccupantModal isOpen={modalOpened} closeModal={closeModal} data={{}} />
+		</>
 	)
 }
 
